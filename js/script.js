8cbe94b84729fff01ac374d514800defefa269ba
Júlia Santos <juliagalvaoalmeidasantos@gmail.com>
2025-03-22 16:54:56 -0300

    section Sobre finalizada, com media e responsiva

--- js/script.js
+++ js/script.js
@@ -46,3 +46,28 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   });
 });
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tab-item');
+  const tabContent = document.querySelectorAll('.tab-content');
+
+  tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+          // Remover a classe 'active' de todos os botões de tab
+          tabs.forEach(t => t.classList.remove('active'));
+
+          // Adicionar a classe 'active' ao botão clicado
+          tab.classList.add('active');
+
+          // Remover a classe 'active' de todos os conteúdos das abas
+          tabContent.forEach(content => content.classList.remove('active'));
+
+          // Mostrar o conteúdo da aba correspondente ao botão clicado
+          const tabId = tab.getAttribute('data-tab');
+          document.getElementById(tabId).classList.add('active');
+      });
+  });
+});
